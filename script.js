9783841f0f8378d510328fe29844a8d6ee71533f
voigi <supervoigi@gmail.com>
2021-02-08 19:07:15 +0100

    ajout son +mise en place footer

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
+//je cibles mes elements //
+    var item = document.querySelector("#test");
+    var item1 = document.querySelector("#test1");
+    var item2 = document.querySelector("#test2");
+    var item3 = document.querySelector("#test3");
 
-var item = document.querySelector("#test");
-var item1 = document.querySelector("#test1");
-var item2 = document.querySelector("#test2");
-var item3 = document.querySelector("#test3");
+//j'instencie un objet audio que je mert dans une variable//
+    var music = new Audio('son/vagues.mp3');
+    var canard = new Audio('son/coin.mp3');
 
-var item4 = document.querySelector(".para");
-item4.textContent="Bacon ipsum dolor amet bacon tongue pork, alcatra tri-tip short ribs biltong picanha boudin cow meatball ham hock ground round.";
+//La variable music utilise la methode loop//
+    //music.loop=true;
+//j'applique des evenements à mes element ciblés plus haut//
+    item.addEventListener("mouseover", playMusic);
+    item.addEventListener("mouseout", stopMusic);
 
-var music = new Audio('son/gold.mp3');
+    item1.addEventListener("mouseover", playMusic);
+    item1.addEventListener("mouseout", stopMusic);
 
-item.addEventListener("mouseover", playMusic, false);
-item.addEventListener("mouseout", stopMusic, false);
+    item2.addEventListener("click", coin);
 
-item1.addEventListener("mouseover", playMusic, false);
-item1.addEventListeners("mouseout", stopMusic, false);
+    item3.addEventListener("click",coin);
+   
+//fonctions jouant et stopant les differents son//
+    function playMusic() {
+        music.play();
+    }
+    function coin() {
+        canard.play();
+    }
 
-item2.addEventListener("mouseover", playMusic, false);
-item2.addEventListener("mouseout", stopMusic, false);
-
-item3.addEventListener("mouseover", playMusic, false);
-item3.addEventListener("mouseout", stopMusic, false);
-
-function playMusic() {
-    music.play();
-}
-
-function stopMusic() {
-    music.pause();
-}
-
-
-
-
+    function stopMusic() {
+        music.pause();
+    }
 
 
